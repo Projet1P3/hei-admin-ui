16959baafd3d9580cbc918cc9b7308641df20df9
Michellah <hei.michellah@gmail.com>
2023-07-30 20:07:50 +0300

    feat: add grade page

--- src/menu/StudentMenu.js
+++ src/menu/StudentMenu.js
@@ -1,20 +1,20 @@
-import { MultiLevelMenu, MenuItemCategory } from '@react-admin/ra-navigation'
-import { useNotify } from 'react-admin'
+import { MultiLevelMenu } from '@react-admin/ra-navigation'
+import { Menu, useNotify } from 'react-admin'
 
 import { Receipt, AttachMoney, AccountCircle } from '@mui/icons-material'
 
 import authProvider from '../providers/authProvider'
 
-export const StudentMenu = () => {
+export const StudentMenu = (props) => {
   const notify = useNotify()
   const notifyNotImplemented = () => notify('En cours de développement. Ce qui présage quelques exercices pour vous 😉', { type: 'warning' })
   const whoamiId = authProvider.getCachedWhoami().id
   return (
-    <MultiLevelMenu variant='categories'>
-      <MenuItemCategory to='/profile' name='profile' label='Mon profil' icon={<AccountCircle />} />
-      <MenuItemCategory to={whoamiId ? `/students/${authProvider.getCachedWhoami().id}/fees` : '/'} name='fees' label='Frais' icon={<AttachMoney />} />
-      <MenuItemCategory to='/' name='student-grades' label='Notes' icon={<Receipt />} onClick={notifyNotImplemented} />
-    </MultiLevelMenu>
+    <Menu {...props} >
+      <Menu.Item to='/profile' name='profile' primaryText='Mon profil' leftIcon={<AccountCircle />} />
+      <Menu.Item to={whoamiId ? `/students/${authProvider.getCachedWhoami().id}/fees` : '/'} name='fees' primaryText='Frais' leftIcon={<AttachMoney />} />
+      <Menu.Item to='/grade' name='student-grades' primaryText='Notes' leftIcon={<Receipt />}  />
+    </Menu>
   )
 }
 
